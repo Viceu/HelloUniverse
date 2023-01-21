bd1f9592df05dfb0227e359c2aaff4ac8e8747f1
jnnfrui <91644664+jnnfrui@users.noreply.github.com>
2023-01-21 18:34:38 -0500

    Adjusting Home Page

--- app/screens/WelcomeScreen.js
+++ app/screens/WelcomeScreen.js
@@ -1,23 +1,30 @@
 import React from 'react';
-import { ImageBackground, StyleSheet, View } from "react-native";
+import { ImageBackground, Button, StyleSheet, View } from "react-native";
 
 
 export default function WelcomeScreen( {navigation} ) {
     return (
-        <ImageBackground 
-        styles = {styles.background}
-        source = {require("../assets/background.png")}
-        >
-        </ImageBackground>
+        <View style = {styles.container}>
+            <Button
+                title = "Curriculum"
+                onPress ={() => navigation.navigate("Curriculum", { content: "curriculum"})}
+            />
+            <Button
+                title = "Library"
+                onPress ={() => navigation.navigate("Library", { content: "Library"})}
+            />
+        </View>
+
     );
 }
 
 //React Native Style Sheet
 const styles = StyleSheet.create({
-    //key-value pairs inside the object  :3
-    background: {
-    flex: 1 // Image background takes up the entire screen 
+    container: {
+        flex: 1,
+        backgroundColor: '#4C698F',
+        alignItems: 'center',
+        justifyContent: 'center'
     }
+    
 })
-
-// export default WelcomeScreen;
