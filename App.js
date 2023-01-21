b4ccef56147e79b597568af1eee5e82c9facb8c5
jnnfrui <91644664+jnnfrui@users.noreply.github.com>
2023-01-21 14:27:25 -0500

    Add existing file

--- App.js
+++ App.js
@@ -1,5 +1,9 @@
 import * as React from 'react';
 import {NavigationContainer, StackActions} from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { Button } from 'react-native';
+
+const Stack = createNativeStackNavigator();
 
 const App = () => {
   return (
@@ -15,5 +19,20 @@ const App = () => {
     </NavigationContainer>
   );
 };
+
+const HomeScreen = ({navigation}) =>{
+  return(
+    <Button
+      title = "Library"
+      onPress={() =>
+        navigation.navigate('Profile', {name: 'Cats'})
+      }
+    />
+    
+  );
+}
+const ProfileScreen = ({navigation, route}) => {
+  return <text>Hello</text>
+}
 
 export default App;
