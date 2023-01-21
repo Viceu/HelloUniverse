ebb0ff6e86502380b16bd440b40547906926a34e
jnnfrui <91644664+jnnfrui@users.noreply.github.com>
2023-01-21 16:08:53 -0500

    Name of Update

--- App.js
+++ App.js
@@ -1,38 +1,7 @@
-import * as React from 'react';
-import {NavigationContainer, StackActions} from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import { Button } from 'react-native';
+import React from 'react';
+import { View } from "react-native";
+import WelcomeScreen from "./app/screens/WelcomeScreen"
 
-const Stack = createNativeStackNavigator();
-
-const App = () => {
-  return (
-    <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen
-        name = "Home"
-        component={HomeScreen}
-        options={{title:'Welcome'}}
-        />
-        <Stack.Screen name="Profile" component={ProfileScreen}/>
-      </Stack.Navigator>
-    </NavigationContainer>
-  );
-};
-
-const HomeScreen = ({navigation}) =>{
-  return(
-    <Button
-      title = "Library"
-      onPress={() =>
-        navigation.navigate('Profile', {name: 'Cats'})
-      }
-    />
-    
-  );
+export default function App() {
+  return <WelcomeScreen />;
 }
-const ProfileScreen = ({navigation, route}) => {
-  return <text>Hello</text>
-}
-
-export default App;
