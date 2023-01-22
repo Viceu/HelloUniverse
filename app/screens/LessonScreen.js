4d4c4c3d5e6b6bddf05dc96b2f21e3bb36f57766
Viceu <vivian.fengfx@gmail.com>
2023-01-21 19:52:11 -0500

    Name of Update

---
+++ app/screens/LessonScreen.js
@@ -0,0 +1,95 @@
+import React from 'react';
+import { ImageBackground, StyleSheet, SafeAreaView, View, Text, Button, Alert, Image, Label} from "react-native";
+
+const App = () => (
+    <SafeAreaView style={styles.container}>
+        <ImageBackground 
+        styles = {styles.background}
+        source = {require("../assets/background.png")}
+        >
+        </ImageBackground>
+        {/* <Button style={styles.labelimg} title=" " onPress={() => Alert.alert('Back')}>
+            <Image source={require("../assets/laptop.png")} />
+        </Button> */}
+        <Text style={styles.label}>
+            Lessons
+        </Text>
+        <Image source={require("../assets/laptop.png")} style={styles.img} />
+        <Text style={styles.question}>
+            Which of these is used to point at things on the monitor?
+        </Text>
+        <View style={styles.space}>
+            <Button style={styles.button}
+                title="Tree"
+                onPress={() => Alert.alert('Incorrect :(')} 
+            />
+        </View>
+        <View style={styles.space}>
+            <Button style={styles.button}
+                title="Laptop"
+                onPress={() => Alert.alert('Correct!')}
+            />
+        </View>
+        <View style={styles.space}>
+            <Button style={styles.button}
+                title="Water Bottle"
+                onPress={() => Alert.alert('Incorrect :(')}
+            />
+        </View>
+        <View style={styles.space}>
+            <Button style={styles.button}
+                title="Cat"
+                onPress={() => Alert.alert('Incorrect :(')}
+            />
+        </View>
+    </SafeAreaView>
+);
+
+//React Native Style Sheet
+const styles = StyleSheet.create({
+    //key-value pairs inside the object
+    background: {
+        flex: 1 // Image background takes up the entire screen 
+    },
+    container: {
+      flex: 1,
+      justifyContent: 'center',
+      marginHorizontal: 16,
+    },
+    label: {
+        position: 'absolute',
+        top:0,
+        marginLeft: 20,
+    },
+    question: {
+        textAlign: 'left',
+        fontWeight: 'bold',
+        marginBottom: 80,
+        fontSize: 20,
+    },
+    title: {
+      textAlign: 'center',
+      marginVertical: 8,
+    },
+    button: {
+        color: "#5CC8FF",
+    },
+    space: {
+        marginBottom: 10,
+    },
+    img: {
+        alignSelf: 'center', 
+        width: 100, 
+        height: 100,
+        marginBottom: 30,
+    },
+    labelimg: {
+        alignSelf: 'left', 
+        width: 20, 
+        height: 20,
+        position: 'absolute',
+        top:0,
+    },
+  });
+  
+  export default App;
--- app/screens/WelcomeScreen.js
+++ app/screens/WelcomeScreen.js
@@ -1,23 +1,49 @@
 import React from 'react';
-import { ImageBackground, StyleSheet, View } from "react-native";
+import { ImageBackground, Button, Image, StyleSheet, View } from "react-native";
 
 
 export default function WelcomeScreen( {navigation} ) {
     return (
-        <ImageBackground 
-        styles = {styles.background}
-        source = {require("../assets/background.png")}
-        >
-        </ImageBackground>
+        <View style = {styles.container}>
+            <ImageBackground 
+            source={'../assets/background.png'} 
+            resizeMode = "cover"
+            style={styles.image}>
+             <Image 
+                source={require('../assets/nametitle.png')}
+                style={{width: 500, height: 100}} />
+            <Image 
+                source={require('../assets/cat_astronaut.png')}
+                style={{width: 300, height: 300}} />
+            <Button
+                title = "let's learn!"
+                color="#fff"
+                onPress ={() => navigation.navigate("Curriculum", { content: "curriculum"})}
+            />
+            <Button
+                title = "library"
+                color="#fff"
+                onPress ={() => navigation.navigate("Library", { content: "Library"})}
+            />
+            </ImageBackground>
+        </View>
+
     );
 }
 
 //React Native Style Sheet
 const styles = StyleSheet.create({
-    //key-value pairs inside the object  :3
-    background: {
-    flex: 1 // Image background takes up the entire screen 
-    }
-})
+    container: {
+        flex: 1,
+        backgroundColor: '#4C698F',
+        alignItems: 'center',
+        justifyContent: 'center'
+    },
+    image: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center'
+    },
 
-// export default WelcomeScreen;
+    
+})
